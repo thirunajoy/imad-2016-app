b2f8685de750867957e3ea3ad7b5a8ea9b33349b
thirunajoy <thirunajoy23@gmail.com>
2016-11-21 21:19:37 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>POST</title>
+   
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="screen">
+       
+       
+    </head>
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+<?php include('dbcon.php'); ?>
